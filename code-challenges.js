c05329998e329f3757e2024c832e1e0ba71560c8
davidkim4 <dkim4016@gmail.com>
2020-10-04 22:28:06 -0700

    assessment complete

--- code-challenges.js
+++ code-challenges.js
@@ -7,8 +7,31 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
+const code = (string) => {
+    //Returns the string argument after splitting it into an array, then mapping through each value(char). If the value is an 'a', 'e', 'i', or 'o', it will replace that letter with a special number assigned each respective letter. Otherwise it will return the normal character and then join the entire array with the replaced values back into a string.
+    return string.split("").map(value => value === 'a' ? 4 : value === 'e' ? 3 : value === 'i' ? 1 : value === 'o' ? 0 : value).join("")
 
-
+    //Refactored this code into the one above to make it look more simplified.
+    // return string.split("").map(value => {
+    //     if (value === 'a') {
+    //         return 4
+    //     }
+    //     else if (value === 'e') {
+    //         return 3
+    //     }
+    //     else if (value === 'i') {
+    //         return 1
+    //     }
+    //     else if (value === 'o') {
+    //         return 0
+    //     }
+    //     else {
+    //         return value
+    //     }
+    // }).join("")
+}
+console.log(code(secretCodeWord1));
+console.log(code(secretCodeWord2));
 
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
@@ -16,8 +39,16 @@ var secretCodeWord2 = "gobbledygook"
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
-
-
+const onlyA = (array) => {
+    //Filters through the array argument. 
+    return array.filter(value => {
+        //Checks each value(word) to see if it includes an 'a' or 'A'. If it does, then it returns the word in a new array.
+        if (value.includes('a') || value.includes('A')) {
+            return value
+        }
+    })
+}
+console.log(onlyA(arrayOfWords));
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -28,3 +59,40 @@ var hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
+var hand4 = [1, 2, 3, 4, 5]
+// Expected output: false
+var hand5 = [5, 5, 3, 3, 3]
+// Expected output: true
+
+const fullHouse = (array) => {
+    //Initialize two numbers (num1 and num2) to be 0 and one array (uniquenNumArr) to be empty.
+    let num1 = 0
+    let num2 = 0
+    let uniqueNumArr = []
+    //Map through array argument and uses ternary to check if uniquNumArr has the same values as array. If uniqueNumArr has a value from the array argument, then it will return nothing, else it will push the value from the array argument into uniqueNumArr.
+    array.map(value => uniqueNumArr.includes(value) ? "" : uniqueNumArr.push(value))
+    //Create a for loop that will loop through each value in array argument.
+    for (let i = 0; i < array.length; i++) {
+        //If statement to compare the first value in uniqueNumArr with all the values in array argument.
+        if (uniqueNumArr[0] === array[i]) {
+            //If condition is true increase num1 by 1.
+            num1++
+        }
+    }
+    //Create another for loop that will loop through each value in array argument.
+    for (let i = 0; i < array.length; i++) {
+        //If statement to compare the second value in uniqueNumArr with all the values in array argument.
+        if (uniqueNumArr[1] === array[i]) {
+            //If condition is true, this time increase num2 by 1.
+            num2++
+        }
+    }
+    //Returns true if there is 1 three of a kind and 1 pair in either num value. Or else it returns false.
+    return num1 == 2 && num2 === 3 || num1 === 3 && num2 === 2 ? true : false
+}
+
+console.log(fullHouse(hand1))
+console.log(fullHouse(hand2))
+console.log(fullHouse(hand3))
+console.log(fullHouse(hand4))
+console.log(fullHouse(hand5))
--- rails-questions.md
+++ rails-questions.md
@@ -2,33 +2,40 @@
 
 1. What is the command to create a new Rails application with a Postgres database?
 
-
+rails new appname -d postgresql -T
 
 2. Explain each section of the following route:  get '/library/:id' => 'book#show'
 
-get -
-library -
-:id -
-book -
-show -
+get - keyword we use to locate the filepath we want to refer to on a localhost:3000 server.
+library - part of the url you name to append to the localhost.
+:id - a parameter that is added to the url. The :id is what you pass into a method.
+book - the file name that holds the controller.
+show - method that is written inside the controller.
 
 
 3. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
+You can generate a new migration by applying $ rails generate migration migration_name. Inside theat migration you can write an add_column command. The syntax for adding a column will be add_column :reference to table, :name of new column, : data type. Once created u call on db:migrate and the schema should update with a new foreign key in the table you assigned it to.
 
 
 
 
 4. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
+rails generate model Person shirt:string pants:string shoes:string
 
 
 
 
 5. I want to add validations to the model from question 4 to ensure no one can make a new entry without data in all the columns. How do I do that?
 
+Inside of your model Person ruby file, you can create a validation checker by using the keyword validates with the column names and presence set to true. 
+
+validates :shirt, :pants, :shoes, presence: true
 
 
 
 
 6. What is the naming convention for generating a Rails model? What is the naming convention for the table that is generated with this command?
+
+The naming convention with a rails model is in CamelCase. The naming convention for a table is in snake_case.
